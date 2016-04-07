9a9d91340df5fe7cce72496675adcd49dd156e70
David McNeil <mcneilde@rose-hulman.edu>
2016-04-07 13:30:24 -0400

    Add: Selection sort

--- src/sort.rs
+++ src/sort.rs
@@ -1,26 +1,55 @@
-pub fn bubble_sort_inplace<T>(vec: &mut Vec<T>)
+fn swap<T>(vec: &mut Vec<T>, i: usize, j: usize)
+	where T: Copy {
+	let tmp = vec[i];
+	vec[i] = vec[j];
+	vec[j] = tmp;
+}
+
+pub fn bubble_inplace<T>(vec: &mut Vec<T>)
 	where T: PartialOrd + Copy {
-	let mut tmp: T;
-	let mut did_swap: bool = true;
+	let mut did_swap = true;
 	while did_swap {
 		did_swap = false;
 		for i in 0..(vec.len() - 1) {
 			if vec[i] > vec[i + 1] {
-				tmp = vec[i];
-				vec[i] = vec[i + 1];
-				vec[i + 1] = tmp;
+				swap(vec, i, i + 1);
 				did_swap = true;
 			}
 		}
 	}
 }
 
-pub fn bubble_sort<T>(vec: &Vec<T>) -> Vec<T>
+pub fn bubble<T>(vec: &Vec<T>) -> Vec<T>
 	where T: PartialOrd + Copy {
 	let mut new_vec = vec.clone();
-	bubble_sort_inplace(&mut new_vec);
+	bubble_inplace(&mut new_vec);
 	new_vec
 }
+
+
+pub fn selection_inplace<T>(vec: &mut Vec<T>)
+	where T: PartialOrd + Copy {
+	let mut minimum_index;
+	for j in 0..(vec.len() - 1) {
+		minimum_index = j;
+		for i in j..vec.len() {
+			if vec[i] < vec[minimum_index] {
+				minimum_index = i;
+			}
+		}
+		if minimum_index != j {
+			swap(vec, j, minimum_index);
+		}
+	}
+}
+
+pub fn selection<T>(vec: &Vec<T>) -> Vec<T>
+	where T: PartialOrd + Copy {
+	let mut new_vec = vec.clone();
+	selection_inplace(&mut new_vec);
+	new_vec
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -40,11 +69,20 @@ mod tests {
 	}
 
     #[test]
-    fn bubble_sort_test() {
+    fn bubble_test() {
         let vec = vec![22, -31, 678, -131, 43, 7, -2];
         let mut sorted_vec = vec.clone();
         sorted_vec.sort();
-        let my_sorted_vec = bubble_sort(&vec);
+        let my_sorted_vec = bubble(&vec);
+        assert!(vectors_equal(&my_sorted_vec, &sorted_vec));
+    }
+
+    #[test]
+    fn selection_test() {
+        let vec = vec![22, -31, 678, -131, 43, 7, -2];
+        let mut sorted_vec = vec.clone();
+        sorted_vec.sort();
+        let my_sorted_vec = selection(&vec);
         assert!(vectors_equal(&my_sorted_vec, &sorted_vec));
     }
 }
